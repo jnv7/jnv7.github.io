3f6caa59bcb9e652ed01b370a71e163daa8beb8c
jnv7 <jnv.reg@gmail.com>
2021-01-02 16:54:42 +0000

    --exclude '.git'

--- lifecycle/deploy.sh
+++ lifecycle/deploy.sh
@@ -6,5 +6,5 @@ echo "Compilar site"
 hugo
 echo "Compilação concluída"
 echo "Instalar site"
-rsync -a -v public/ /var/www/jnvasconcelos.com/site
+rsync -a -v --exclude '.git*' public/ /var/www/jnvasconcelos.com/site
 echo "Instalação concluída"
